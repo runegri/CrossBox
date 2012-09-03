23d84f6a867c9a6f958c220e8eff9e3acbb062d2
runegri <rag@rag.no>
2012-09-03 09:50:02 +0200

    Listing of folder contents works properly on iOS

--- CrossBox.UI.iOS/Views/MainMenuView.cs
+++ CrossBox.UI.iOS/Views/MainMenuView.cs
@@ -4,6 +4,10 @@ using CrossBox.Core.ViewModels;
 using Cirrious.MvvmCross.Interfaces.ServiceProvider;
 using Cirrious.MvvmCross.Binding.Interfaces;
 using Cirrious.MvvmCross.Views;
+using MonoTouch.UIKit;
+using Cirrious.MvvmCross.Binding.Touch.ExtensionMethods;
+using System.Collections.Generic;
+using MonoTouch.Foundation;
 
 namespace CrossBox.UI.iOS
 {
@@ -21,7 +25,51 @@ namespace CrossBox.UI.iOS
 
 			Title = "CrossBox";
 
+			var tableSource = new TableViewSource(TableView);
+
+			this.AddBindings(new Dictionary<object, string>
+			                 {
+								{ tableSource, "{'ItemsSource':{'Path':'FolderContents'}}"}
+							 });
+
+			TableView.Source = tableSource;
+			TableView.ReloadData();
+
 			//Bind("Title", this, "FolderName", null);
+		}
+
+
+		public class TableViewSource : MvxBindableTableViewSource 
+		{
+			readonly static NSString CellId = new NSString("DropBoxItemTableCell");
+
+
+			public TableViewSource(UITableView tableView) : base(tableView)
+			{}
+
+			protected override UITableViewCell GetOrCreateCellFor (UITableView tableView, MonoTouch.Foundation.NSIndexPath indexPath, object item)
+			{
+				var reuseCell = tableView.DequeueReusableCell(CellId);
+				if(reuseCell != null)
+				{
+					return reuseCell;
+				}
+
+				var newCell = new TableViewCell(UITableViewCellStyle.Subtitle, CellId);
+				return newCell;
+			}
+
+		}
+
+		public class TableViewCell : MvxBindableTableViewCell
+		{
+			const string Binding = @"{'TitleText':{'Path':'Name'}}";
+
+			public TableViewCell(UITableViewCellStyle style, NSString cellId) : 
+				base(Binding, style, cellId)
+			{
+				Accessory = UITableViewCellAccessory.DisclosureIndicator;
+			}
 		}
 	}
 }
